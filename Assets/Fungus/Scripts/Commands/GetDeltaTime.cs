c7df3c7f886e4fb18569471b96ebeeedfe3537f5
Alexa Thanos <80543657+akthanos@users.noreply.github.com>
2022-11-10 16:29:53 -0800

    Playing with Backgrounds

--- Assets/Fungus/Scripts/Commands/GetDeltaTime.cs
+++ Assets/Fungus/Scripts/Commands/GetDeltaTime.cs
@@ -7,7 +7,7 @@ namespace Fungus
         "Place in Update to get the time in seconds since the last frame the computer renders. Use a Fungus Float var to store this value.")]
     [AddComponentMenu("")]
 
-    public class GetDeltaTime: Command
+    public class GetDeltaTime : Command
     {
         [Tooltip("Use the arrow to the right and make a Fungus Float variable to be able to use the Time.deltaTime value")]
         [SerializeField] protected FloatData deltaTimeVar = new FloatData(0f);
