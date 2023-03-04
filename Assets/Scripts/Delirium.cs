c974fb444dba7353d496be11d3cc98dc88fcec11
Alexa Thanos <80543657+akthanos@users.noreply.github.com>
2023-03-03 16:28:30 -0800

    straggler

--- Assets/Scripts/Delirium.cs
+++ Assets/Scripts/Delirium.cs
@@ -3,39 +3,5 @@ using UnityEngine.UI;
 
 public class Delirium : MonoBehaviour
 {
-    public Text deliriumText;
-    public Image deliriumBar;
 
-    float delirium, maxDelirium = 10;
-
-    private void Start()
-    {
-        delirium = maxDelirium;
-    }
-    private void Update()
-    {
-        deliriumText.text = delirium + "";
-        if (delirium > maxDelirium) delirium = maxDelirium;
-
-        DeliriumBarFiller();
-    }
-
-    void DeliriumBarFiller()
-    {
-        deliriumBar.fillAmount = delirium / maxDelirium;
-    }
-
-    public void Delirious(float deliriousPoints)
-    {
-        if (delirium > 0)
-            delirium -= deliriousPoints;
-    }
-
-    public void Sane(float sanePoints)
-    {
-        if (delirium < maxDelirium)
-            delirium += sanePoints;
-    }
 }
-
-
