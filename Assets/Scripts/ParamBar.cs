96a90da4791d808b7d9b389ec973e0f0892e37f2
Alexa Thanos <80543657+akthanos@users.noreply.github.com>
2023-03-13 03:47:32 -0700

    Zeus added, FMOD updated

---
+++ Assets/Scripts/ParamBar.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class ParamBar : MonoBehaviour
+{
+    [HideInInspector] public int _band;
+    [SerializeField] private float _startScale = 1f;
+    [SerializeField] private float _scaleMultipler = 10f;
+
+    private float startPos;
+    private float previousPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.y;
+        previousPos = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float resizeAmount = FMODAudioVisualizer.bandBuffer[_band] * _scaleMultipler;
+
+        transform.position = new Vector3(transform.position.x, startPos, transform.position.z);
+        transform.localScale = new Vector3(transform.localScale.x, resizeAmount + _startScale, transform.localScale.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y + resizeAmount / 2, transform.position.z);
+    }
+}
+
+//script modified from Scott Game Sounds
+// https://scottgamesounds.com/c-scripts/
